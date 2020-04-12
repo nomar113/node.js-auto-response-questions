4160af501ca0325ff1695ee61bb2cc63bd68d368
Ramon Mesquita <ramonmesquita113@gmail.com>
2020-04-12 00:14:26 -0300

    Respondendo as questões e refactoring nas constantes

--- src/index.js
+++ src/index.js
@@ -1,31 +1,41 @@
 const { Builder, By, Key, until } = require('selenium-webdriver');
 const credentials = require('./credentials.json');
+const informations = require('./informations.json');
 
 (async function example() {
 	let driver = await new Builder().forBrowser('firefox').build();
-	let URL_UNICARIOCA = 'http://ava.unicarioca.edu.br';
-	let URL_EXERCICIOS = 'https://ava.unicarioca.edu.br/graduacao/mod/quiz/view.php?id=';
-	let ID_EXERCICIOS = [508832, /*508838*/];
-	let matricula = credentials.matricula;
-	let senha = credentials.senha;
+	let ID_EXERCICIOS = [508832, 508838, 508844, 508850, 508856, 508864, 508870,
+		508876, 508882, 508888, 508896];
 	let respostas = [];
-	const stringRespostaCorreta = 'A resposta correta é: ';
 	try {
-		await driver.get(URL_UNICARIOCA);
-		await driver.findElement(By.name('username')).sendKeys(matricula, Key.TAB);
-		await driver.findElement(By.name('password')).sendKeys(senha, Key.TAB, Key.TAB, Key.SPACE);
-		await driver.wait(until.titleIs('UniCarioca Graduação'), 600000);
+		await driver.get(informations.urlAva);
+		await driver.findElement(By.name('username')).sendKeys(credentials.matricula, Key.TAB);
+		await driver.findElement(By.name('password')).sendKeys(credentials.senha, Key.TAB, Key.TAB, Key.SPACE);
+		await driver.wait(until.titleIs(informations.title), 600000);
 		for (id of ID_EXERCICIOS) {
-			await driver.get(URL_EXERCICIOS + id);
+			await driver.get(informations.urlAvaQuestion + id);
 			let nota;
 			try {
 				nota = await driver.findElement(By.css('div#feedback > h3')).getText()
 			} catch {
 				nota = null;
 			}
-			while (nota !== 'Nota mais alta: 5,00 / 5,00.') {
+			while (!(nota !== informations.grade5 || nota !== informations.grade10)) {
 				// Continuar tentativa || Tentar novamente
 				await driver.findElement(By.tagName('button')).click();
+				// Responde questionário
+				if (respostas.length > 0) {
+					let alternativas = await driver.findElements(By.css('label.m-l-1'));
+					for (let i = 0; i < alternativas.length; i++) {
+						for (let j = 0; j < respostas.length; j++) {
+							let textoAlternativa = await alternativas[i].getText();
+							if (textoAlternativa.includes(respostas[j])) {
+								alternativas[i].click();
+							}
+						}
+					}
+				}
+				respostas = [];
 				// Finalizar tentativa... 
 				let botaoFinalizarTentativa = await driver.findElement(By.name('next'));
 				console.log(await botaoFinalizarTentativa.isDisplayed());
@@ -37,11 +47,10 @@ const credentials = require('./credentials.json');
 				let buttons = await driver.findElements(By.css('button.btn.btn-secondary'));
 				buttons[1].click();
 				// Enviar tudo e terminar (modal, botão vermelho)
-				await driver.sleep(10000);
+				await driver.sleep(3000);
 				try {
+					//Só aparece na primeira vez que envia o questionário
 					let redButton = await driver.findElement(By.css('input.btn.btn-primary'));
-					console.log(redButton);
-					await driver.sleep(4000);
 					redButton.click();
 				} catch {
 					console.log('Não tem botão vermelho!');
@@ -50,20 +59,18 @@ const credentials = require('./credentials.json');
 				let rightAnswers = await driver.findElements(By.css('div.rightanswer'));
 				for (let i = 0; i < rightAnswers.length; i++) {
 					let respostaHtml = await rightAnswers[i].getText();
-					console.log(respostaHtml);
-					let respostaFormatada = respostaHtml.slice(stringRespostaCorreta.length);
-					console.log(respostaFormatada);
+					let respostaFormatada = respostaHtml.slice(informations.answerRight.length);
+					respostaFormatada = respostaFormatada.slice(0, -1);
 					respostas.push(respostaFormatada);
 				}
 				console.log('respostas: ')
 				console.log(respostas);
-				// TO DO: clicar em terminar revisão e remover o break
-				break;
+				await (await driver.findElement(By.linkText(informations.finishRevision))).click();
 				nota = await driver.findElement(By.css('div#feedback > h3')).getText();
 			}
 			console.log(nota)
 		}
 	} finally {
-		// await driver.quit()
+		await driver.quit()
 	}
 })();
